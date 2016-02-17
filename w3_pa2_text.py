f1b12f0a332c617c0ceaf436df919764c0528873
krokodim <dmitry.grekov@gmail.com>
2016-02-17 08:21:51 +0300

    im

--- w3_pa2_text.py
+++ w3_pa2_text.py
@@ -1,6 +1,27 @@
 from sklearn import datasets
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.svm import SVC
+from sklearn.grid_search import GridSearchCV
+from sklearn.cross_validation import KFold
+import numpy as np
+
 
 newsgroups = datasets.fetch_20newsgroups(
                     subset='all',
                     categories=['alt.atheism', 'sci.space']
              )
+
+vv = TfidfVectorizer()
+
+X = vv.fit_transform(newsgroups.data)
+y = newsgroups.target
+
+
+grid = {'C': np.power(10.0, np.arange(-5, 6))}
+cv = KFold(y.size, n_folds=5, shuffle=True, random_state=241)
+clf = SVC(kernel='linear', random_state=241)
+gs = GridSearchCV(clf, grid, scoring='accuracy', cv=cv)
+gs.fit(X, y)
+
+for a in gs.grid_scores_:
+    print a.mean_validation_score, '<<', a.parameters
